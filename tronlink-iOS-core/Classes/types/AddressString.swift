226f8449f1d9c43fa799148cad74417d2fd03e27
sunny <sunnypanda.li@tron.network>
2026-03-28 12:49:36 +0800

    fix crash from force-unwrap in hexStringToUTF8Data

--- tronlink-iOS-core/Classes/types/AddressString.swift
+++ tronlink-iOS-core/Classes/types/AddressString.swift
@@ -50,7 +50,7 @@ public extension String {
         let regex = try! NSRegularExpression(pattern: "[0-9a-f]{1,2}", options: .caseInsensitive)
         regex.enumerateMatches(in: self, range: NSMakeRange(0, utf16.count)) { match, flags, stop in
             let byteString = (self as NSString).substring(with: match!.range)
-            var num = UInt8(byteString, radix: 16)!
+            guard var num = UInt8(byteString, radix: 16) else { return }
             data.append(&num, count: 1)
         }
         
